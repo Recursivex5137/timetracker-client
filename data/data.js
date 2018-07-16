86a6d26a6b590645cc3c70333b78e8d8a03dda6a
recursivex5137 <recursivex5137@gmail.com>
2018-07-16 14:59:55 -0400

    Init Commit.

---
+++ data/data.js
@@ -0,0 +1,15 @@
+export const exampleData = {
+  timeTypes: [
+    'RegularTime',
+    'MiscellaneousTime',
+    'CleanTime'
+  ],
+  timesArray: [
+    { StartTime: '', EndTime: '', TotalTime: '', timeType: 'RegularTime' } // ISO 8601
+  ],
+  CurrentUser: {
+    firstName: 'Gerald',
+    lastName: 'Eicher',
+    AuthenticationToken: 'ajsdojfosdih3243ohioih342343',
+  }
+}
