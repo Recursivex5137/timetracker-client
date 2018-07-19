cccfc9009d00d2c8c031bca5ae661430e025158d
recursivex5137 <recursivex5137@gmail.com>
2018-07-19 16:09:38 -0400

    Added global Styles.

---
+++ components/GlobalStyle.js
@@ -0,0 +1,19 @@
+import { StyleSheet } from 'react-native';
+
+const globalStyles = StyleSheet.create({
+  columnLayout: {
+    flex: 1,
+    flexDirection: 'column'
+  },
+  paddingRight20: {
+    paddingRight: 20
+  },
+  paddingLeft20: {
+    paddingLeft: 20
+  },
+  padding10: {
+    padding: 10
+  }
+});
+
+export { globalStyles }
